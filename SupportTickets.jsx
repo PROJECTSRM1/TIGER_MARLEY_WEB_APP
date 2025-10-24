6b74ad2ba44e352ff2f78322d3353a06950dd654
VenkataRamanaRM1 <venkataramana.bolli@rm1codershub.com>
2025-10-24 10:58:29 +0530

    Add files via upload

---
+++ SupportTickets.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SupportTickets = () => {
+  return (
+    <div>
+      <h2>Support Tickets Page</h2>
+      <p>All Support Tickets will appear here.</p>
+    </div>
+  );
+};
+
+export default SupportTickets;
